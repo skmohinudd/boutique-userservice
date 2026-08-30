83f9ae563b1e43be0ecb0cab231ab4dbb00a9c2c
Shaik Khaja Mohinuddin <shaikmohinuddin799@gmail.com>
2026-08-31 02:25:43 +0530

    chore: sync project changes - 2026-08-31 02:25:43

--- src/main/java/com/boutique/user/config/LocalSecurityConfig.java
+++ src/main/java/com/boutique/user/config/LocalSecurityConfig.java
@@ -1,6 +1,6 @@
 package com.boutique.user.config;
 import org.springframework.context.annotation.*;import org.springframework.security.config.annotation.web.builders.HttpSecurity;import org.springframework.security.web.SecurityFilterChain;
-@Configuration @Profile("!aws")
+@Configuration @Profile("local")
 public class LocalSecurityConfig {
  @Bean SecurityFilterChain localSecurity(HttpSecurity http)throws Exception{return http.csrf(csrf->csrf.disable()).authorizeHttpRequests(auth->auth.anyRequest().permitAll()).build();}
 }
